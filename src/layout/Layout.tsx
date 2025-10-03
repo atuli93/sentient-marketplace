0510cf3a67f0d8aeb03375c43b75bd82f3aadb24
Atul Kumar <atul.chieff60@gmail.com>
2025-10-03 05:51:43 +0530

    Update Layout.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -12,9 +12,22 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="main-layout">
       <Navbar />
-      <main className="main-content">
-        {children}
-      </main>
+
+      <div className="page-layout">
+        <main className="main-content">
+          {children}
+        </main>
+
+        <aside className="filter-sidebar">
+          <h4>Recently Listed</h4>
+          <h4>Filters</h4>
+          <h4>Price</h4>
+          <h4>Status</h4>
+          <h4>Collections</h4>
+          <h4>Rarity</h4>
+        </aside>
+      </div>
+
       <Chatbot />
     </div>
   );
