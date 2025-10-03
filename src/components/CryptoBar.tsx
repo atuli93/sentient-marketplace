4b09cf8c0b2d194a3450ba19dea9cf0bf60b6392
Atul Kumar <atul.chieff60@gmail.com>
2025-10-03 05:39:13 +0530

    Update CryptoBar.tsx

--- src/components/CryptoBar.tsx
+++ src/components/CryptoBar.tsx
@@ -34,4 +34,36 @@ const CryptoBar: React.FC = () => {
   return (
     <div className="crypto-bar">
       <div className="crypto-info">
-        <span>ETH: {da
+        <span>ETH: {data.ethPrice}</span>
+        <span>Gas: {data.gasPrice}</span>
+        <span className="status">
+          {data.status === 'Live' && <span className="live-indicator" />}
+          {data.status}
+        </span>
+      </div>
+
+      <div className="support-section">
+        <strong>Support</strong>
+        <div>Name: Atul (your name - atulchief)</div>
+        <div>
+          Twitter:{' '}
+          <a href="https://x.com/Chief_atul" target="_blank" rel="noreferrer">
+            @Chief_atul
+          </a>
+        </div>
+        <div>
+          GitHub:{' '}
+          <a href="https://github.com/atuli93" target="_blank" rel="noreferrer">
+            atuli93
+          </a>
+        </div>
+        <div>
+          Email:{' '}
+          <a href="mailto:atul.chieff60@gmail.com">atul.chieff60@gmail.com</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CryptoBar;
